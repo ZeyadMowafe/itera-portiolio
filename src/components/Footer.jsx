ec51a6c1c92a79afdce102a5c60f5c1a5b541595
FAROUKDEV7 <faroukeldreinydev74@gmail.com>
2026-03-28 07:02:48 +0200

    update social media links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -20,10 +20,22 @@ const footerLinks = {
     { label: "Dashboards", href: "#services", internal: true },
   ],
   Connect: [
-    { label: "GitHub", href: "#", internal: false },
-    { label: "LinkedIn", href: "#", internal: false },
-    { label: "Twitter", href: "#", internal: false },
-    { label: "Telegram", href: "#", internal: false },
+    {
+      label: "GitHub",
+      href: "https://github.com/iteracodes-dev",
+      internal: false,
+    },
+    {
+      label: "LinkedIn",
+      href: "https://www.linkedin.com/company/itera-dev/",
+      internal: false,
+    },
+    {
+      label: "Instagram",
+      href: "https://www.instagram.com/itera.dev/",
+      internal: false,
+    },
+    { label: "Telegram", href: "https://t.me/itera_dev", internal: false },
   ],
 };
 
@@ -149,7 +161,8 @@ export default function Footer() {
   const handleLinkClick = (e, link) => {
     e.preventDefault();
     if (!link.internal) {
-      if (link.href !== "#") window.open(link.href, "_blank", "noopener,noreferrer");
+      if (link.href !== "#")
+        window.open(link.href, "_blank", "noopener,noreferrer");
       return;
     }
     if (window.location.pathname === "/") {
@@ -157,7 +170,9 @@ export default function Footer() {
     } else {
       navigate("/");
       setTimeout(() => {
-        document.querySelector(link.href)?.scrollIntoView({ behavior: "smooth" });
+        document
+          .querySelector(link.href)
+          ?.scrollIntoView({ behavior: "smooth" });
       }, 400);
     }
   };
@@ -248,18 +263,18 @@ export default function Footer() {
               {[
                 {
                   name: "GitHub",
-                  href: "#",
+                  href: "https://github.com/iteracodes-dev",
                   path: "M12 0C5.37 0 0 5.37 0 12c0 5.31 3.435 9.795 8.205 11.385.6.105.825-.255.825-.57 0-.285-.015-1.23-.015-2.235-3.015.555-3.795-.735-4.035-1.41-.135-.345-.72-1.41-1.23-1.695-.42-.225-1.02-.78-.015-.795.945-.015 1.62.87 1.845 1.23 1.08 1.815 2.805 1.305 3.495.99.105-.78.42-1.305.765-1.605-2.67-.3-5.46-1.335-5.46-5.925 0-1.305.465-2.385 1.23-3.225-.12-.3-.54-1.53.12-3.18 0 0 1.005-.315 3.3 1.23.96-.27 1.98-.405 3-.405s2.04.135 3 .405c2.295-1.56 3.3-1.23 3.3-1.23.66 1.65.24 2.88.12 3.18.765.84 1.23 1.905 1.23 3.225 0 4.605-2.805 5.625-5.475 5.925.435.375.81 1.095.81 2.22 0 1.605-.015 2.895-.015 3.3 0 .315.225.69.825.57A12.02 12.02 0 0024 12c0-6.63-5.37-12-12-12z",
                 },
                 {
                   name: "LinkedIn",
-                  href: "#",
+                  href: "https://www.linkedin.com/company/itera-dev/",
                   path: "M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433a2.062 2.062 0 01-2.063-2.065 2.064 2.064 0 112.063 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z",
                 },
                 {
-                  name: "Twitter",
-                  href: "#",
-                  path: "M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z",
+                  name: "Instagram",
+                  href: "https://www.instagram.com/itera.dev/",
+                  path: "M7.75 2C4.574 2 2 4.574 2 7.75v8.5C2 19.426 4.574 22 7.75 22h8.5C19.426 22 22 19.426 22 16.25v-8.5C22 4.574 19.426 2 16.25 2h-8.5zm0 1.5h8.5c2.347 0 4.25 1.903 4.25 4.25v8.5c0 2.347-1.903 4.25-4.25 4.25h-8.5c-2.347 0-4.25-1.903-4.25-4.25v-8.5c0-2.347 1.903-4.25 4.25-4.25zm4.25 3.5a4.75 4.75 0 100 9.5 4.75 4.75 0 000-9.5zm0 1.5a3.25 3.25 0 110 6.5 3.25 3.25 0 010-6.5zm5.25-.88a1.12 1.12 0 100 2.24 1.12 1.12 0 000-2.24z",
                 },
               ].map((s, i) => (
                 <a
@@ -273,7 +288,12 @@ export default function Footer() {
                   onMouseEnter={() => handleSocialEnter(i)}
                   onMouseLeave={() => handleSocialLeave(i)}
                 >
-                  <svg width="15" height="15" viewBox="0 0 24 24" fill="currentColor">
+                  <svg
+                    width="15"
+                    height="15"
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                  >
                     <path d={s.path} />
                   </svg>
                 </a>
